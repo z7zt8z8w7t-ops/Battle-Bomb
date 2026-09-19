f66e490216348d817411ed1a1b54f30c16b632eb
z7zt8z8w7t-ops <z7zt8z8w7t@privaterelay.appleid.com>
2026-09-19 15:28:42 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='battle-bomb-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
